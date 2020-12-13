ebffb83263fcca1bd555af71ad81a49f73da9e42
grim <dima.ggrimbergg@gmail.com>
2020-12-13 21:33:27 +0300

    Scripted auto-commit on change (2020-12-13 21:33:27) by gitwatch.sh

--- src/main/java/ru.parLab/SparkJob.java
+++ src/main/java/ru.parLab/SparkJob.java
@@ -7,5 +7,7 @@ public class SparkJob {
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> distAirportDelays = sc.textFile()
     }
 }
