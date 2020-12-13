1fc28b4ab2fa0011fea5c3f7997ff922ca53c149
grim <dima.ggrimbergg@gmail.com>
2020-12-13 22:49:53 +0300

    Scripted auto-commit on change (2020-12-13 22:49:53) by gitwatch.sh

--- src/main/java/ru.parLab/SerializableFlightCounters.java
+++ src/main/java/ru.parLab/SerializableFlightCounters.java
@@ -1,7 +1,67 @@
 package ru.parLab;
 
-public class SerializableFlightCounters {
+//Требуется определить для пары <аэропорт отлета, аэропорт прибытия>
+//максимальное время опоздания, процент опоздавших+отмененных рейсов.
+//Также требуется связать полученную таблицу с названиями аэропортов.
+
+import java.io.Serializable;
+
+public class SerializableFlightCounters implements Serializable {
     private float maxDelay;
+    private int countOfCancelled;
+    private int countOfDelayed;
+    private int flightCount;
+
+    public SerializableFlightCounters(){}
+
+    public SerializableFlightCounters(float maxDelay, int countOfCancelled, int countOfDelayed, int flightCount){
+        this.maxDelay = maxDelay;
+        this.countOfCancelled = countOfCancelled;
+        this.countOfDelayed = countOfDelayed;
+        this.flightCount = flightCount;
+    }
+
+
+    public float getMaxDelay(){
+        return this.maxDelay;
+    }
+
+    public int getCountOfCancelled(){
+        return this.countOfCancelled;
+    }
+
+    public int getCountOfDelayed(){
+        return this.countOfDelayed;
+    }
+
+    public int getFlightCount(){
+        return this.flightCount;
+    }
+
+    public static SerializableFlightCounters addValue(float maxDelay, boolean wasDelayed, boolean wasCancelled,SerializableFlightCounters ptrToSFC){
+        return new SerializableFlightCounters(
+                Float.max(maxDelay, ptrToSFC.getMaxDelay()),
+                wasCancelled ? ptrToSFC.getCountOfCancelled() + 1 : ptrToSFC.getCountOfCancelled(),
+                wasDelayed ? ptrToSFC.getCountOfDelayed() + 1 : ptrToSFC.getCountOfDelayed(),
+                ptrToSFC.getFlightCount() + 1);
+    }
+
+    public static SerializableFlightCounters split(SerializableFlightCounters left, SerializableFlightCounters right){
+        return new SerializableFlightCounters(
+                Float.max(left.getMaxDelay(), right.getMaxDelay()),
+                left.getCountOfCancelled() + right.getCountOfCancelled(),
+                left.getCountOfDelayed() + right.getCountOfDelayed(),
+                left.getFlightCount() + right.getFlightCount());
+    }
+
+    public static outAsString(SerializableFlightCounters ptrToSFC){
+        return "[Max delay:" + ptrToSFC.getMaxDelay() +
+                " Count of delays: " + ptrToSFC.getCountOfDelayed() +
+                " Count of cancelled: " + ptrToSFC.getCountOfCancelled() +
+                "percent of delays: " + (float)((100 * ptrToSFC.getCountOfDelayed() / ptrToSFC.getFlightCount())) +
+                "percent of canceled: " + ((float)(100 * ptrToSFC.getCountOfCancelled() / ptrToSFC.getFlightCount())) +
+                " ] ";
+    }
 
 
 }
